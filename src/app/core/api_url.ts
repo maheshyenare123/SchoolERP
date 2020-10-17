7e976a96d1872f86ee5743ff335cf45a04830a3b
Vision Chakan <ashuhb1994@gmail.com>
2020-10-17 13:49:51 +0530

    Revert "Add Logs And Api url"
    This reverts commit f0e74c274023ef65280c3bcab92d96453447691e.

--- src/app/core/api_url.ts
+++ src/app/core/api_url.ts
@@ -1,26 +1,26 @@
 export class Constants {
     public static URL: any = {
-        HOST_URL: 'http://yamistha.cloudjiffy.net/api/',
-       accessToken:'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdXBlcmFkbWluQGdtYWlsLmNvbSIsIlNlc3Npb25JRCI6MSwiaWF0IjoxNjAyOTIxMjk1LCJleHAiOjE2MDMwMDc2OTV9.9yG8Kaw2H3rW_x36syT2HZbznn8KbjjrNNkiw29WpJ3TA_HFnixPRJuFcRZfN9DL_83rCV7ZXtvK7wzLwcu1RA',
+        HOST_URL: 'http://yamistha.cloudjiffy.net/',
+       accessToken:'',
        sessionId:'1',
 
     }
 
     public static Front_Office: any = {
-       Admission_Enquiry:'enquiry',
-       Complain:'complaint',
-       ComplainType:'complaint-type',
-       Phone_Call_Log:'general-call',
-       Postal_Dispatch:'dispatch-receive',
-       Postal_Receive:'dispatch-receive',
-       Visitor_Book:'visitors-book',
-       Visitor_Purpose:'visitor-purpose',
-       Reference:'reference',
-       Source:'source',
+       Admission_Enquiry:'api/enquiry',
+       Complain:'',
+       ComplainType:'',
+       Phone_Call_Log:'',
+       Postal_Dispatch:'',
+       Postal_Receive:'',
+       Visitor_Book:'',
+       Purpose:'',
+       Reference:'',
+       Source:'',
     }
 
     public static Student_Information: any = {
-        Disable_Reason:'disableReason',
+        Disable_Reason:'api/disableReason',
         Is_Active : '',
         Reason : '',
      }
