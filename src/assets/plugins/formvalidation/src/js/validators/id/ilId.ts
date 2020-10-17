83ceb98e5e960123ad4cb5ab62b1da4c9eb3fcfc
Vision Chakan <ashuhb1994@gmail.com>
2020-10-17 11:50:16 +0530

    unchange

--- src/assets/plugins/formvalidation/src/js/validators/id/ilId.ts
+++ src/assets/plugins/formvalidation/src/js/validators/id/ilId.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * FormValidation (https://formvalidation.io)
  * The best validation library for JavaScript
@@ -27,33 +26,3 @@ export default function ilId(value: string) {
         valid: luhn(value),
     };
 }
-=======
-/**
- * FormValidation (https://formvalidation.io)
- * The best validation library for JavaScript
- * (c) 2013 - 2020 Nguyen Huu Phuoc <me@phuoc.ng>
- */
-
-import luhn from '../../algorithms/luhn';
-
-/**
- * Validate Israeli identity number (Mispar Zehut)
- *
- * @see https://gist.github.com/freak4pc/6802be89d019bca57756a675d761c5a8
- * @see http://halemo.net/info/idcard/
- * @returns {ValidateResult}
- */
-export default function ilId(value: string) {
-    if (!/^\d{1,9}$/.test(value)) {
-        return {
-            meta: {},
-            valid: false,
-        };
-    }
-
-    return {
-        meta: {},
-        valid: luhn(value),
-    };
-}
->>>>>>> d05e719b8d76eea2e2bfb31a974d47e8096a290b
--- src/assets/plugins/formvalidation/src/js/validators/vat/rsVat.ts
+++ src/assets/plugins/formvalidation/src/js/validators/vat/rsVat.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * FormValidation (https://formvalidation.io)
  * The best validation library for JavaScript
@@ -37,43 +36,3 @@ export default function rsVat(value: string) {
         valid: (sum + parseInt(v.substr(8, 1), 10)) % 10 === 1,
     };
 }
-=======
-/**
- * FormValidation (https://formvalidation.io)
- * The best validation library for JavaScript
- * (c) 2013 - 2020 Nguyen Huu Phuoc <me@phuoc.ng>
- */
-
-/**
- * Validate Serbian VAT number
- *
- * @returns {ValidateResult}
- */
-export default function rsVat(value: string) {
-    let v = value;
-    if (/^RS[0-9]{9}$/.test(v)) {
-        v = v.substr(2);
-    }
-    if (!/^[0-9]{9}$/.test(v)) {
-        return {
-            meta: {},
-            valid: false,
-        };
-    }
-
-    let sum = 10;
-    let temp = 0;
-    for (let i = 0; i < 8; i++) {
-        temp = (parseInt(v.charAt(i), 10) + sum) % 10;
-        if (temp === 0) {
-            temp = 10;
-        }
-        sum = (2 * temp) % 11;
-    }
-
-    return {
-        meta: {},
-        valid: (sum + parseInt(v.substr(8, 1), 10)) % 10 === 1,
-    };
-}
->>>>>>> d05e719b8d76eea2e2bfb31a974d47e8096a290b
